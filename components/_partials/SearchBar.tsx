2ddd6acf74f4edf6fbdcc5e56287efceec6575b5
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-01-20 10:35:04 +0100

    Search Icon

--- components/_partials/SearchBar.tsx
+++ components/_partials/SearchBar.tsx
@@ -1,11 +1,14 @@
+import { SearchIcon } from "@heroicons/react/outline";
+
 export default function SearchBar() {
   return (
-    <div className="rounded bg-gray-100 text-sm w-full">
+    <div className="rounded flex bg-gray-100 text-sm w-full px-2 items-center justify-between">
       <input
         className="bg-transparent w-full p-2 text-dark-50 focus:outline-none"
         placeholder="Search courses or modules"
-        type="search"
       />
+
+      <SearchIcon className="h-5 w-5" />
     </div>
   );
 }
